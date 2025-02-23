c3b7ca24a67b1dd989fee392cf2aefa79d17352a
Lucas Portella <lucas.portellasantos@gmail.com>
2025-02-23 12:13:11 -0300

    add guard to evaluate division remainder

--- fizzbuzz/lib/fizzbuzz.ex
+++ fizzbuzz/lib/fizzbuzz.ex
@@ -8,8 +8,19 @@ defmodule Fizzbuzz do
   def handle_file_read({:ok, result}) do
     result
     |> String.split(",")
-    |> Enum.map(&String.to_integer/1)
+    |> Enum.map(&convert_and_evaluate/1)
   end
 
   def handle_file_read({:error, reason}), do: "Error reading the file: #{reason}"
+
+  def convert_and_evaluate(elem) do
+    elem
+    |> String.to_integer()
+    |> evaluate_numbers()
+  end
+
+  def evaluate_numbers(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
+  def evaluate_numbers(number) when rem(number, 3) == 0, do: :fizz
+  def evaluate_numbers(number) when rem(number, 5) == 0, do: :buzz
+  def evaluate_numbers(number), do: number
 end
